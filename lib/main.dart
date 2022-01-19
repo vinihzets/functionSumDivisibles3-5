75b7343d70bb45c19eb011540ae59ebcab9dbce5
Vinicius Almeida <viniciusdaarky@outlook.com>
2022-01-19 14:07:13 -0300

    Desafio-01

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+
+  int multipleNumber(int value) {
+    var sum = 0;
+    for (int i = 0; i < value; i++) {
+      if (i % 3 == 0 || i % 5 == 0) {
+        sum += i;
+      }
+    }
+    return sum;
+  }
+
+  int value = multipleNumber(20);
+  print(value);
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
